60d16f55e5b5214dcbc0e5ee10fe5b71f3b96277
Jero Pitkänen <terajero@gmail.com>
2026-08-31 20:31:47 +0300

    3.15-3.18 tehty

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -46,51 +46,54 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
   }
 ]*/
 
-// 3.2 tehtävä
-const info = () => {
+// 3.2 tehtävä, poistettu käytöstä 3.18
+/*const info = () => {
   const date = new Date()
   return Person.countDocuments({}).then(count => {
     return `Phonebook has info for ${count} people <br><br>${date}`
   })
-}
+}*/
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-  response.json(persons)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-  response.send(info())
+app.get('/info', (request, response, next) => {
+  Person.countDocuments({})
+    .then(count => {
+      response.send(`Phonebook has info for ${count} people <br><br>${new Date()}`)
+    })
+    .catch(error => next(error))
 })
 
-// 3.3 tehtävä
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+// 3.3 tehtävä, 3.18 tehtävässä muutettu
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-// 3.4 tehtävä
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const personIndex = persons.findIndex(person => person.id === id)
-
-    if (personIndex !== -1) {
-    persons.splice(personIndex, 1)
-    response.status(204).end()
-  } else {
-    response.status(404).end()
-  }
+// 3.4 tehtävä, 3.15 muutettu
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // 3.5 ja 3.6 tehtävä, muutoksia tietokannan lisäämisen jälkeen
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -105,7 +108,41 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.status(201).json(savedPerson)
   })
+    .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = body.name
+      person.number = body.number
+
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
+// 3.16 tehtävä
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// 3.16 tehtävä
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
